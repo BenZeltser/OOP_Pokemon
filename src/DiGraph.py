add9f279ca1d07829880a6fba70e193e1d514088
Tomer Levy <tomerlevy10@gmail.com>
2022-01-05 16:19:57 +0200

    Rewrting Digraph 2

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -16,9 +16,23 @@ class DiGraph(GraphInterface):
         self.modCounter=0
 
 #this function returns the amount of vertices in a graph
-    def v_size (self):
+    def v_size (self) ->int:
         return len(self.vertices)
 
+#this function retruns the amount of edges in a graph
+    def e_size (self)-> int:
+        counter=0
+        for i in self.edges.keys():
+            counter+= len(self.edges.get(i))
+        return counter
 
+#this function returns a dictionary of all the vertices that are in a graph
+    def get_all_v(self) -> dict:
+        return self.vertices
+
+
+#this function returns a dictionary of the all vertices connected from id1
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        return self.edges.get(id1)
 
 
